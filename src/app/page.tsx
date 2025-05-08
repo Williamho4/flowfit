cf2af25cca6d1eb3574c81c8e5ec51a5f6f2e1e6
Williamho4 <willyho_@hotmail.se>
2025-05-08 02:24:40 +0200

    css-for-home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import MultiSteps from '@/components/multi-steps'
+import WorkoutList from '@/components/workout-list'
+import { getWorkout } from '@/lib/server-utils'
 import { getSession } from '@/lib/session'
 import { redirect } from 'next/navigation'
+import styles from '@/styles/home-page.module.css'
 
 export default async function Home() {
   const session = await getSession()
@@ -8,6 +11,28 @@ export default async function Home() {
   if (!session) {
     redirect('/login')
   }
+  const Workout = await getWorkout(session.user.id, 1)
 
-  return
+  console.log(Workout.data?.exercises)
+
+  if (!Workout.data?.exercises) {
+    return null
+  }
+
+  return (
+    <>
+      {' '}
+      {Workout && (
+        <div className={styles.container}>
+          <div className={styles.container__left}>
+            <WorkoutList workout={Workout.data.exercises} />
+            <WorkoutList workout={Workout.data.exercises} />
+          </div>
+          <div className={styles.container__right}>
+            <WorkoutList workout={Workout.data.exercises} />
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
