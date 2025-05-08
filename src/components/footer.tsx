65c92ea09837b3314820e4927d97a21470e199b7
Williamho4 <willyho_@hotmail.se>
2025-05-08 02:29:36 +0200

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <small>&copy; 2050 ByteGrad. All rights reserved</small>
+      <small>&copy; 2050 FlowFit. All rights reserved</small>
     </footer>
   )
 }
