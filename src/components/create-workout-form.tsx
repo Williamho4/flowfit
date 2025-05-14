90c0a61daf5f5cb4b7710f94305b8ac070492d51
Williamho4 <willyho_@hotmail.se>
2025-05-14 21:28:01 +0200

    workout-planner-responsive

--- src/components/create-workout-form.tsx
+++ src/components/create-workout-form.tsx
@@ -1,19 +1,19 @@
-import { createWorkout } from "@/lib/server-utils";
-import { BaseExercise } from "@prisma/client";
-import { useForm } from "react-hook-form";
-import styles from "@/styles/create-workout-form.module.css";
-import { UserInfo } from "@/lib/types";
+import { createWorkout } from '@/lib/server-utils'
+import { BaseExercise } from '@prisma/client'
+import { useForm } from 'react-hook-form'
+import styles from '@/styles/create-workout-form.module.css'
+import { UserInfo } from '@/lib/types'
 
 type Inputs = {
-  title: string;
-  date: Date;
-};
+  title: string
+  date: Date
+}
 
 type CreateWorkoutFormProps = {
-  workout: BaseExercise[] | [];
-  setWorkout: React.Dispatch<React.SetStateAction<BaseExercise[] | []>>;
-  user: UserInfo;
-};
+  workout: BaseExercise[] | []
+  setWorkout: React.Dispatch<React.SetStateAction<BaseExercise[] | []>>
+  user: UserInfo
+}
 
 export default function CreateWorkoutForm({
   workout,
@@ -26,7 +26,7 @@ export default function CreateWorkoutForm({
     setError,
     reset,
     formState: { errors, isSubmitting, isSubmitSuccessful },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>()
 
   const handleAddWorkout = (
     title: string,
@@ -35,40 +35,42 @@ export default function CreateWorkoutForm({
     workouts: BaseExercise[]
   ) => {
     if (workout.length <= 0) {
-      return setError("root", {
-        message: "Please add atleast one exercise",
-      });
+      return setError('root', {
+        message: 'Please add atleast one exercise',
+      })
     }
-    createWorkout(title, date, userId, workouts);
-    reset();
-    setWorkout([]);
-  };
+    createWorkout(title, date, userId, workouts)
+    reset()
+    setWorkout([])
+  }
 
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        const { title, date } = data;
+    <section className={styles.container}>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          const { title, date } = data
 
-        handleAddWorkout(title, date, user.id, workout);
-      })}
-      className={styles.dashboard__inputs}
-    >
-      <input
-        {...register("title", { required: "This field is required" })}
-        placeholder="title"
-      />
-      <p>{errors.title?.message}</p>
-      <input
-        {...register("date", { required: "This field is required" })}
-        placeholder="date"
-        type="date"
-      />
-      <p>{errors.date?.message}</p>
-      <button type="submit" disabled={isSubmitting}>
-        {isSubmitting ? "Adding workout" : "Add workout"}
-      </button>
-      {errors.root && <p>{errors.root.message}</p>}
-      {isSubmitSuccessful && <p>Workout added successfully</p>}
-    </form>
-  );
+          handleAddWorkout(title, date, user.id, workout)
+        })}
+        className={styles.dashboard__inputs}
+      >
+        <input
+          {...register('title', { required: 'This field is required' })}
+          placeholder="title"
+        />
+        <p>{errors.title?.message}</p>
+        <input
+          {...register('date', { required: 'This field is required' })}
+          placeholder="date"
+          type="date"
+        />
+        <p>{errors.date?.message}</p>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Adding workout' : 'Add workout'}
+        </button>
+        {errors.root && <p>{errors.root.message}</p>}
+        {isSubmitSuccessful && <p>Workout added successfully</p>}
+      </form>
+    </section>
+  )
 }
--- src/components/workout-planner.tsx
+++ src/components/workout-planner.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client'
 
-import styles from "@/styles/workout-planner.module.css";
-import BaseExerciseList from "@/components/base-exercise-list";
-import SelectedExercisesList from "@/components/selected-exercises-list";
-import SelectedExercise from "./selected-exercise";
-import { BaseExercise } from "@prisma/client";
-import { useState } from "react";
-import { UserInfo } from "@/lib/types";
-import CreateWorkoutForm from "./create-workout-form";
+import styles from '@/styles/workout-planner.module.css'
+import BaseExerciseList from '@/components/base-exercise-list'
+import SelectedExercisesList from '@/components/selected-exercises-list'
+import SelectedExercise from './selected-exercise'
+import { BaseExercise } from '@prisma/client'
+import { useState } from 'react'
+import { UserInfo } from '@/lib/types'
+import CreateWorkoutForm from './create-workout-form'
 
 type WorkoutPlannerProps = {
-  baseExercises: BaseExercise[] | undefined;
-  user: UserInfo;
-};
+  baseExercises: BaseExercise[] | undefined
+  user: UserInfo
+}
 
 export default function WorkoutPlanner({
   baseExercises,
@@ -20,50 +20,52 @@ export default function WorkoutPlanner({
 }: WorkoutPlannerProps) {
   const [selectedExercise, setSelectedExercise] = useState<BaseExercise | null>(
     null
-  );
-  const [workout, setWorkout] = useState<BaseExercise[] | []>([]);
-  const [exerciseError, setExerciseError] = useState<string | null>(null);
+  )
+  const [workout, setWorkout] = useState<BaseExercise[] | []>([])
+  const [exerciseError, setExerciseError] = useState<string | null>(null)
 
   const handleAddExercise = (exercise: BaseExercise) => {
     if (workout.some((e) => e.id === exercise.id)) {
-      return setExerciseError("Exercise already added");
+      return setExerciseError('Exercise already added')
     }
 
-    setWorkout((prev) => [...prev, exercise]);
-  };
+    setWorkout((prev) => [...prev, exercise])
+  }
 
   const handleDeleteExercise = (id: number) => {
-    setWorkout((prev) => prev.filter((exercise) => exercise.id !== id));
-  };
+    setWorkout((prev) => prev.filter((exercise) => exercise.id !== id))
+  }
 
   return (
     <div className={styles.dashboard}>
-      <div className={styles.dashboard__lists}>
+      <div className={styles['item-1']}>
         <BaseExerciseList
           baseExercises={baseExercises}
           setSelected={setSelectedExercise}
           setError={setExerciseError}
         />
+      </div>
+      <div className={styles['item-2']}>
         <SelectedExercisesList
           workout={workout}
           handleDelete={(id) => handleDeleteExercise(id)}
         />
       </div>
-      <div className={styles.dashboard__actions}>
+      <div className={styles['item-3']}>
         <SelectedExercise
           selectedExercise={selectedExercise}
           handleAddExercise={handleAddExercise}
           setSelected={setSelectedExercise}
           error={exerciseError}
         />
-        <div className={styles.dashboard__form}>
-          <CreateWorkoutForm
-            setWorkout={setWorkout}
-            workout={workout}
-            user={user}
-          />
-        </div>
+      </div>
+      <div className={styles['item-4']}>
+        <CreateWorkoutForm
+          setWorkout={setWorkout}
+          workout={workout}
+          user={user}
+        />
       </div>
     </div>
-  );
+  )
 }
