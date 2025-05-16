e21635831b4b4ad0bf50eaa6bc29525cd11e0abf
Williamho4 <willyho_@hotmail.se>
2025-05-16 20:46:46 +0200

    fixed-email-bug

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,47 +1,47 @@
-'use server'
+"use server";
 
-import { redirect } from 'next/navigation'
-import { comparePassword, hashPassword } from './auth-utils'
-import prisma from './db'
-import { createSession, deleteSession } from './session'
+import { redirect } from "next/navigation";
+import { comparePassword, hashPassword } from "./auth-utils";
+import prisma from "./db";
+import { createSession, deleteSession } from "./session";
 
 export async function createUser(
   username: string,
   email: string,
   password: string
 ) {
-  const hashedPassword = await hashPassword(password)
+  const hashedPassword = await hashPassword(password);
 
   const user = await prisma.user.create({
     data: {
       username: username.toLowerCase(),
-      email,
+      email: email.toLowerCase(),
       password: hashedPassword,
     },
-  })
+  });
 
-  return user
+  return user;
 }
 
 export async function loginUser(email: string, password: string) {
   const user = await prisma.user.findUnique({
     where: { email: email.toLowerCase() },
-  })
+  });
   if (!user) {
-    return { ok: false, message: 'Invalid credentials' }
+    return { ok: false, message: "Invalid credentials" };
   }
 
-  const isValid = await comparePassword(password, user.password)
+  const isValid = await comparePassword(password, user.password);
   if (!isValid) {
-    return { ok: false, message: 'Invalid credentials' }
+    return { ok: false, message: "Invalid credentials" };
   }
 
-  await createSession(user)
+  await createSession(user);
 
-  redirect('/')
+  redirect("/");
 }
 
 export async function logout() {
-  await deleteSession()
-  redirect('/login')
+  await deleteSession();
+  redirect("/login");
 }
