37160c25071cf68cc1c6c8385d4f45cbf981e629
Williamho4 <willyho_@hotmail.se>
2025-05-16 22:14:20 +0200

    changed-ai-model

--- src/lib/ai-utils.ts
+++ src/lib/ai-utils.ts
@@ -1,27 +1,27 @@
-import OpenAI from 'openai'
+import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.AI_API_KEY,
-})
+});
 
 export async function calculateCaloriesBurnt(data: unknown) {
-  const workoutString = JSON.stringify(data, null, 2)
+  const workoutString = JSON.stringify(data, null, 2);
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
+    model: "gpt-4o",
     messages: [
       {
-        role: 'system',
+        role: "system",
         content: `You are a fitness assistant. Given a list of exercises with reps, weight, weight is in kilos, the weight is for every rep, estimate total calories burned using this logic:
         - Only return a single rounded number (integer). No units, no extra words.
         -Bench Press: 30 reps in totla with 50 kg is 68 calories use this as example`,
       },
       {
-        role: 'user',
+        role: "user",
         content: `${workoutString}`,
       },
     ],
-  })
+  });
 
-  return response.choices[0].message.content
+  return response.choices[0].message.content;
 }
