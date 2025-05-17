91fb06d52e24aa0097306d148e71cf1834920bc1
Williamho4 <willyho_@hotmail.se>
2025-05-17 02:09:21 +0200

    Added supsense to stats

--- src/app/workout/[id]/page.tsx
+++ src/app/workout/[id]/page.tsx
@@ -1,78 +1,47 @@
-import { getWorkout } from '@/lib/workout-server-utils'
-import { getSession } from '@/lib/session'
-import { redirect } from 'next/navigation'
-import styles from '@/styles/workout-page.module.css'
-import WorkoutEditor from '@/components/workout/id/workout-editor'
-import Stats from '@/components/ui/stats'
-import { calculateCaloriesBurnt } from '@/lib/ai-utils'
+import { getWorkout } from "@/lib/workout-server-utils";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
+import styles from "@/styles/workout-page.module.css";
+import WorkoutEditor from "@/components/workout/id/workout-editor";
+import Stats from "@/components/ui/stats";
+import { Suspense } from "react";
 
 type PageProps = {
-  params: Promise<{ id: string }>
-}
+  params: Promise<{ id: string }>;
+};
 
 export default async function Page({ params }: PageProps) {
-  const { id } = await params
-  const session = await getSession()
+  const { id } = await params;
+  const session = await getSession();
 
   if (!session) {
-    redirect('/login')
+    redirect("/login");
   }
 
-  const res = await getWorkout(session.user.id, Number(id))
+  const res = await getWorkout(session.user.id, Number(id));
 
   if (!res.data) {
-    return redirect('/')
+    return redirect("/");
   }
-
-  const { exercises } = res.data
-  const exerciseNameAndSets = exercises.map((exercise) => ({
-    name: exercise.baseExercise.name,
-    sets: exercise.sets.map((set) => ({ reps: set.reps, weight: set.weight })),
-  }))
-  const caloriesBurnt = await calculateCaloriesBurnt(exerciseNameAndSets)
-
-  const totalSets = res.data?.exercises.reduce((acc, exercise) => {
-    return acc + exercise.sets.length
-  }, 0)
-
-  const totalReps = res.data?.exercises.reduce((acc, exercise) => {
-    const setReps = exercise.sets.reduce((acc, set) => {
-      return acc + set.reps
-    }, 0)
-    return acc + setReps
-  }, 0)
-
-  const totalWeightLifted = res.data?.exercises.reduce((acc, exercise) => {
-    const setWeight = exercise.sets.reduce((acc, set) => {
-      return acc + set.reps * set.weight
-    }, 0)
-
-    return acc + setWeight
-  }, 0)
-
-  const doneExercises = res.data?.exercises.reduce((acc, exercise) => {
-    if (exercise.sets.length > 0) {
-      return acc + 1
-    } else return acc
-  }, 0)
 
   return (
     <>
       {res.data ? (
         <div className={styles.container}>
           <WorkoutEditor workout={res.data.exercises} />
-          <Stats
-            caloriesBurnt={caloriesBurnt}
-            totalSets={totalSets}
-            totalReps={totalReps}
-            totalWeightLifted={totalWeightLifted}
-            value={doneExercises ? doneExercises : 0}
-            maxValue={res.data.exercises.length ? res.data.exercises.length : 0}
-          />
+          <Suspense
+            fallback={
+              <section className={styles["stats-container"]}>
+                Loading...
+              </section>
+            }
+          >
+            <Stats exercises={res.data.exercises} />
+          </Suspense>
         </div>
       ) : (
         <div>Cant fint workout</div>
       )}
     </>
-  )
+  );
 }
--- src/components/ui/stats.tsx
+++ src/components/ui/stats.tsx
@@ -1,23 +1,44 @@
-import styles from '@/styles/stats.module.css'
-import ProgressBar from './progress-bar'
+import styles from "@/styles/stats.module.css";
+import ProgressBar from "./progress-bar";
+import { calculateCaloriesBurnt } from "@/lib/ai-utils";
+import { Exercise } from "@/lib/types";
 
 type StatsProps = {
-  totalSets: number | undefined
-  totalReps: number | undefined
-  totalWeightLifted: number | undefined
-  value: number
-  maxValue: number
-  caloriesBurnt: string | null
-}
+  exercises: Exercise[];
+};
 
-export default function Stats({
-  totalSets,
-  totalReps,
-  totalWeightLifted,
-  value,
-  maxValue,
-  caloriesBurnt,
-}: StatsProps) {
+export default async function Stats({ exercises }: StatsProps) {
+  const exerciseNameAndSets = exercises.map((exercise) => ({
+    name: exercise.baseExercise.name,
+    sets: exercise.sets.map((set) => ({ reps: set.reps, weight: set.weight })),
+  }));
+  const caloriesBurnt = await calculateCaloriesBurnt(exerciseNameAndSets);
+
+  const totalSets = exercises.reduce((acc, exercise) => {
+    return acc + exercise.sets.length;
+  }, 0);
+
+  const totalReps = exercises.reduce((acc, exercise) => {
+    const setReps = exercise.sets.reduce((acc, set) => {
+      return acc + set.reps;
+    }, 0);
+    return acc + setReps;
+  }, 0);
+
+  const totalWeightLifted = exercises.reduce((acc, exercise) => {
+    const setWeight = exercise.sets.reduce((acc, set) => {
+      return acc + set.reps * set.weight;
+    }, 0);
+
+    return acc + setWeight;
+  }, 0);
+
+  const doneExercises = exercises.reduce((acc, exercise) => {
+    if (exercise.sets.length > 0) {
+      return acc + 1;
+    } else return acc;
+  }, 0);
+
   return (
     <section className={styles.container}>
       <div className={styles.info}>
@@ -27,8 +48,11 @@ export default function Stats({
         <p>{`Total weight lifted this workout: ${totalWeightLifted}`}</p>
       </div>
       <div className={styles.progress}>
-        <ProgressBar value={value} maxValue={maxValue} />
+        <ProgressBar
+          value={doneExercises ? doneExercises : 0}
+          maxValue={exercises.length}
+        />
       </div>
     </section>
-  )
+  );
 }
