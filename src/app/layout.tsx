a23a57b1c8ae29dbded4908831ebcda19857316a
Williamho4 <willyho_@hotmail.se>
2025-05-20 03:33:38 +0200

    changed-font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import StoreProvider from "./StoreProvider";
-import Container from "@/components/container";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import { getSession } from "@/lib/session";
+import type { Metadata } from 'next'
+import './globals.css'
+import StoreProvider from './StoreProvider'
+import Container from '@/components/container'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import { getSession } from '@/lib/session'
+import { Poppins } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "FlowFit",
-  description: "Log your workouts",
-};
+  title: 'FlowFit',
+  description: 'Log your workouts',
+}
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  const session = await getSession();
+  const session = await getSession()
 
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>
         <StoreProvider initialSession={session ? session.user : null}>
           <Container>
@@ -30,5 +36,5 @@ export default async function RootLayout({
         </StoreProvider>
       </body>
     </html>
-  );
+  )
 }
